95e268c3d6c3ad70b14cc8e0b9c39a6608857c92
5qU4llV777 <166067521+5qU4llV777@users.noreply.github.com>
2026-06-19 16:18:17 -0300

    db sucess

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -1,4 +1,6 @@
-import { PrismaClient } from "@prisma/client";
+import "dotenv/config";
+import { PrismaClient } from ".prisma/client";
+
 const prisma = new PrismaClient();
 
 async function main() {
